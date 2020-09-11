0216cfdd0f0fc3aeca1b565126b7fb98c7cc7639
abhshekprakash256 <you@example.com>
2020-09-11 14:32:30 +0530

    Braces

--- lex.yy.c
+++ lex.yy.c
@@ -287,10 +287,10 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 
 #define YY_NUM_RULES 8
 #define YY_END_OF_BUFFER 9
-static yyconst short int yy_accept[21] =
+static yyconst short int yy_accept[18] =
     {   0,
-        0,    0,    9,    8,    7,    1,    5,    4,    4,    4,
-        2,    4,    3,    4,    0,    0,    2,    3,    6,    0
+        0,    0,    9,    8,    7,    1,    4,    5,    5,    2,
+        3,    0,    0,    2,    3,    6,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -304,11 +304,11 @@ static yyconst int yy_ec[256] =
        13,   14,    1,    1,   15,   15,   15,   15,   15,   15,
        15,   15,   15,   15,   15,   15,   15,   15,   15,   15,
        15,   15,   15,   15,   15,   15,   15,   15,   15,   15,
-        1,    1,    1,    1,    1,    1,   15,   15,   15,   15,
+       16,    1,   17,    1,    1,    1,   15,   15,   15,   15,
 
        15,   15,   15,   15,   15,   15,   15,   15,   15,   15,
        15,   15,   15,   15,   15,   15,   15,   15,   15,   15,
-       15,   15,    1,    1,    1,    1,    1,    1,    1,    1,
+       15,   15,   18,    1,   19,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -325,42 +325,40 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[16] =
+static yyconst int yy_meta[20] =
     {   0,
         1,    1,    2,    1,    1,    1,    1,    3,    1,    3,
-        1,    1,    1,    1,    3
+        1,    1,    1,    1,    3,    1,    1,    1,    1
     } ;
 
-static yyconst short int yy_base[23] =
+static yyconst short int yy_base[20] =
     {   0,
-        0,    0,   28,   29,   29,   29,   29,   29,    9,   11,
-        0,   29,    0,   29,   17,   13,    0,    0,   15,   29,
-       23,   14
+        0,    0,   29,   30,   30,   30,   30,   30,   11,    0,
+        0,   18,   13,    0,    0,   17,   30,   23,   22
     } ;
 
-static yyconst short int yy_def[23] =
+static yyconst short int yy_def[20] =
     {   0,
-       20,    1,   20,   20,   20,   20,   20,   20,   20,   20,
-       21,   20,   22,   20,   20,   20,   21,   22,   20,    0,
-       20,   20
+       17,    1,   17,   17,   17,   17,   17,   17,   17,   18,
+       19,   17,   17,   18,   19,   17,    0,   17,   17
     } ;
 
-static yyconst short int yy_nxt[45] =
+static yyconst short int yy_nxt[50] =
     {   0,
-        4,    5,    6,    7,    7,    8,    9,   10,    4,    4,
-       11,   12,    8,    8,   13,   14,   18,   15,   16,   15,
-       16,   15,   16,   17,   19,   17,   19,   20,    3,   20,
-       20,   20,   20,   20,   20,   20,   20,   20,   20,   20,
-       20,   20,   20,   20
+        4,    5,    6,    7,    7,    8,    9,    9,    4,    4,
+       10,    8,    8,    8,   11,    7,    7,    7,    7,   12,
+       13,   12,   13,   14,   15,   14,   16,   16,   17,    3,
+       17,   17,   17,   17,   17,   17,   17,   17,   17,   17,
+       17,   17,   17,   17,   17,   17,   17,   17,   17
     } ;
 
-static yyconst short int yy_chk[45] =
+static yyconst short int yy_chk[50] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    9,   22,    9,    9,   10,
-       10,   16,   16,   21,   19,   21,   15,    3,   20,   20,
-       20,   20,   20,   20,   20,   20,   20,   20,   20,   20,
-       20,   20,   20,   20
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    9,
+        9,   13,   13,   18,   19,   18,   16,   12,    3,   17,
+       17,   17,   17,   17,   17,   17,   17,   17,   17,   17,
+       17,   17,   17,   17,   17,   17,   17,   17,   17
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -396,7 +394,7 @@ int line =0;
 int line2 = 0;
 int line3 =0;
 
-#line 400 "lex.yy.c"
+#line 398 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -560,7 +558,7 @@ YY_DECL
 
 #line 24 "file_01.l"
 
-#line 564 "lex.yy.c"
+#line 562 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -611,13 +609,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 21 )
+				if ( yy_current_state >= 18 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 29 );
+		while ( yy_base[yy_current_state] != 30 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -666,7 +664,7 @@ YY_RULE_SETUP
 case 5:
 YY_RULE_SETUP
 #line 29 "file_01.l"
-{printf("%s:\n",yytext);}
+{addtokens(yytext, linenumber);}
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
@@ -683,7 +681,7 @@ YY_RULE_SETUP
 #line 37 "file_01.l"
 ECHO;
 	YY_BREAK
-#line 687 "lex.yy.c"
+#line 685 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -975,7 +973,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 21 )
+			if ( yy_current_state >= 18 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1010,11 +1008,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 21 )
+		if ( yy_current_state >= 18 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 20);
+	yy_is_jam = (yy_current_state == 17);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1585,12 +1583,7 @@ int main(int argc[], char *argv[] )
                fp1= fopen("test_case_01.txt", "r");
                yyin=fp1;
                yylex();
-               //printf("Total number of tokens = %c\n",n_x);
-               //printf("Total number of tokens = %d\n",n_y);
-               //printf("Total number of tokens = %d\n",n_z);
-               //printf("Total number of tokens = %d\n",line);
-               //printf("Total number of tokens = %d\n",line2);
-               //printf("Total number of tokens = %d\n",line3);
+               
                
                return 0;
 }
