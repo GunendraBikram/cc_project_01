8d5843cd16bf4de8a101942ac79fa4310bd65783
Abhi <abhi@pop-os.localdomain>
2020-10-23 04:14:51 +0530

    demo

---
+++ symbol_table.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include"containers.h"
+#include<stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+
+
+int main()
+{
+	struct ast *head;
+	struct ast *second;
+	struct ast *third;
+
+	head->id= 7;
+	//head->token = "ID";
+	//head->is_leaf= 0;
+	//head->ntoken= 2;
+	//head->fun= "get-int";
+	//head->child= 
+	//head->next= second;
+	//second->id = 11;
+	//second->next= third;
+	//third->id = 4;
+	//third->next= NULL;
+
+
+	return 0;
+}
+
+
+
+
+
